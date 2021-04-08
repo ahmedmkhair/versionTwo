87506e906e79f6901c72cd4a810e7fb9a5d2380c
ahmedmkhair <81166810+ahmedmkhair@users.noreply.github.com>
2021-04-08 16:03:08 +0300

    Update check.test.js

--- check.test.js
+++ check.test.js
@@ -15,7 +15,7 @@ const connection = async () => new Promise(
             host: 'localhost',
             user: 'ahmed',
             password: '12class34',
-            database: 'apitest'
+            database: 'etdb'
         });
         connection.connect(error => {
             if (error) {
