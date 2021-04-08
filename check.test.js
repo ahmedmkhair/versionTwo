e8715773da8c6b5d3adefa54eabbe6020276a652
ahmedmkhair <81166810+ahmedmkhair@users.noreply.github.com>
2021-04-08 15:55:19 +0300

    Update check.test.js

--- check.test.js
+++ check.test.js
@@ -43,18 +43,21 @@ const testConnection = async (con, qu) => {
     con.end()
     return result
 }
-
-(async () => {
-    await testConnection(await connection(), `delete from chat`)
-    await testConnection(await connection(), `delete from messages`)
-})()
-
-//testing case title
+let chatId = null
+const cleanDBTables = async () =>{
+    await testConnection(await connection(), `DELETE from messages WHERE sen = '77777777'`)
+    await testConnection(await connection(), `DELETE from messages WHERE sen = '77777778'`)
+    let c = await testConnection(await connection(), `select * from chat`)
+    chatId = c.length + 100000
+    await testConnection(await connection(), `DELETE from chat WHERE user_one_num = '77777777'`)
+}
+cleanDBTables()
+// //testing case title
 test('Test login successfuly', async () => {
 
     // login with user and password to get a token
     let res = await axios.post('http://localhost:3000/api/login', {
-        username: 'seif',
+        username: 'testUserSen',
         password: '123'
     }
     ).catch(function (e) {
@@ -68,7 +71,7 @@ test('failed to login', async () => {
 
     // login with user and password to get a token
     let res = await axios.post('http://localhost:3000/api/login', {
-        username: 'seif',
+        username: 'testUserSen',
         password: '12345'
     }
     ).catch(function (error) {
@@ -79,14 +82,14 @@ test('failed to login', async () => {
 
 test('get friend list', async () => {
     let res = await axios.post('http://localhost:3000/api/login', {
-        username: 'seif',
+        username: 'testUserSen',
         password: '123'
     }
     ).catch(function (e) {
         expect(e.response.status).toBe(401)
     })
     let token = res.data
-    await testConnection(await connection(), `insert into chat values (1, '66666662', '66666663')`)
+    await testConnection(await connection(), `insert into chat (id, user_one_num, user_two_num) values (${chatId}, '77777777', '77777778')`)
     let getdata = await axios.get("http://localhost:3000/api/v1.0/contacts",
         {
             headers: {
@@ -96,14 +99,15 @@ test('get friend list', async () => {
             expect(e.response.status).toBe(403)
         })
     let info = [getdata.data.contacts[0].name, getdata.data.contacts[0].phoneNum]
-    expect(info).toEqual(['ahmed', '66666663'])
+
+    expect(info).toEqual(['testUserRec', '77777778'])
 })
 
 test('send message', async () => {
 
     // login with user and password to get a token
     let res = await axios.post('http://localhost:3000/api/login', {
-        username: 'seif',
+        username: 'testUserSen',
         password: '123'
     })
 
@@ -111,21 +115,23 @@ test('send message', async () => {
     expect(res.status).toBe(200)
 
 
-    let messages = await testConnection(await connection(), `select * from messages`)
+    let messages = await testConnection(await connection(), `select * from messages where sen = '77777777'`)
     expect(messages.length).toBe(0)
 
+    let chatCounter = await testConnection(await connection(), `select * from chat`)
+
     let getdata = await axios.post('http://localhost:3000/api/sendMessage', {
-        sender: "66666662",
+        sender: "77777777",
         message: "hello from tester",
-        receiver: "66666663",
-        cid: "1",
+        receiver: "77777778",
+        cid: chatId,
         headers: {
             Authorization: `Bearer ${token}`
         }
     })
 
     // check the db if it got the message
-    messages = await testConnection(await connection(), `select * from messages`)
+    messages = await testConnection(await connection(), `select * from messages where sen = '77777777'`)
     expect(messages.length).toBe(1)
     expect(messages[0].message).toEqual('hello from tester')
     expect(getdata.status).toBe(200)
@@ -133,7 +139,7 @@ test('send message', async () => {
 
 test('get chat', async () => {
     let res = await axios.post('http://localhost:3000/api/login', {
-        username: 'seif',
+        username: 'testUserSen',
         password: '123'
     }
     ).catch(function (e) {
@@ -142,14 +148,13 @@ test('get chat', async () => {
     let token = res.data
 
     let getdata = await axios.post('http://localhost:3000/api/getConv', {
-        sender: "66666662",
-        receiver: "66666663",
+        sender: "77777777",
+        receiver: "77777778",
         headers: {
             Authorization: `Bearer ${token}`
         }
     })
-    
-    // check the api if it reads the messages correctly
-    expect(getdata.data.userChat[0].message).toBe('hello from tester')
-})
 
+    expect(getdata.data.userChat[0].message).toBe('hello from tester')
+    cleanDBTables()
+})
