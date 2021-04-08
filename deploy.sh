fea8e3d2d7224c1e9a9f2ff313278ab1bb05553d
ahmedmkhair <81166810+ahmedmkhair@users.noreply.github.com>
2021-04-08 17:09:22 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-rm /opt/project/*
-rm /var/www/project/*
-cp server.js /opt/project
-cp installserver.sh /opt/project
-chmod +x /opt/project/installserver.sh
-cp clientfiles/* /var/www/project
-bash /opt/project/installserver.sh
+sudo rm /opt/project/*
+sudo rm /var/www/project/*
+sudo cp server.js /opt/project
+sudo cp installserver.sh /opt/project
+sudo chmod +x /opt/project/installserver.sh
+sudo cp clientfiles/* /var/www/project
+sudo bash /opt/project/installserver.sh
