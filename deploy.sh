4e3ea7c293b8df351929e90a8aef8ffb82bc91aa
ahmedmkhair <81166810+ahmedmkhair@users.noreply.github.com>
2021-04-08 17:35:45 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo pm2 delete all
 sudo rm /opt/project/*
 sudo rm /var/www/project/*
 sudo cp server.js /opt/project
 sudo cp installserver.sh /opt/project
 sudo chmod +x /opt/project/installserver.sh
 sudo cp clientfiles/* /var/www/project
+pm2 delete all
 pm2 start /opt/project/server.js
