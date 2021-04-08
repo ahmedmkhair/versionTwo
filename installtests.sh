e4db58abaae64269ff739b4370f9f1ccccb31900
ahmedmkhair <81166810+ahmedmkhair@users.noreply.github.com>
2021-04-08 17:40:34 +0300

    Update installtests.sh

--- installtests.sh
+++ installtests.sh
@@ -8,4 +8,4 @@ sudo npm install puppeteer --unsafe-perm=true
 sudo npm install jsonwebtoken
 sudo npm install express
 sudo npm install bcryptjs
-sudo pm2 start server.js
+sudo pm2 restart server.js
